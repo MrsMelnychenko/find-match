924bade4e762e284c142b0d8e934d25556fe8322
Mariana <melnychenko.mariana@gmail.com>
2023-01-11 22:01:22 +0200

    managed to add img to cards randomly

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ const easyLevel = document.querySelector(".easy-level");
 const mediumLevel = document.querySelector(".medium-level");
 const hardLevel = document.querySelector(".hard-level");
 const cardContainer = document.querySelector(".container");
-const cardBack = document.querySelectorAll('.back');
+const cardBack = document.querySelector('.back');
+const card = document.querySelector('.card');
 
 //Function to create timer
 let minute = 0;
@@ -38,14 +39,6 @@ function returnData(input) {
 }
 startBtn.addEventListener('click', start);
 
-// Function to pick random img for card
-// function randomImg() {
-//    let random = Math.floor(Math.random()*21);
-//   cardBack.forEach(elem => elem.style.backgroundImage = `url('/img/${random}.png')`);
-// }
-
-// cardBack.addEventListener('click', randomImg());
-
 //Function to change grid depending from Choosed level
 function changeGrid() {
   let grid = 0;
@@ -59,17 +52,17 @@ function changeGrid() {
     }
     }
   if (mediumLevel.checked === true) {
-    grid = 30;
+    grid = 10;
     document.querySelector(".container").innerHTML = "";
     cardContainer.style.gridTemplate = 'repeat(5, 70px) / repeat(6, 70px)';
     for (let i = 0; i < grid; i++) {
-      cardContainer.insertAdjacentHTML('beforeEnd', '<div class="card"><div class="front"></div><div class="back"></div></div>');
-      let random = Math.floor(Math.random()*21);
-  cardBack.forEach(elem => elem.style.backgroundImage = `url('/img/${random}.png')`);
+      cardContainer.insertAdjacentHTML('beforeEnd', `<div class="card" id="${i}"><div class="front"></div><div class="back"></div></div>`);
+      let random = Math.floor(Math.random() * 21);
+      document.getElementById(i).style.backgroundImage = `url('/img/${random}.png')`;
     }
     }
   if (hardLevel.checked === true) {
-    grid = 42;
+    grid = 20;
     document.querySelector(".container").innerHTML = "";
         cardContainer.style.gridTemplate = 'repeat(6, 70px) / repeat(7, 70px)';
     for (let i = 0; i < grid; i++) {
@@ -81,5 +74,8 @@ mediumLevel.addEventListener("click", changeGrid);
 hardLevel.addEventListener("click", changeGrid);
 easyLevel.addEventListener("click", changeGrid);
 
-
-
+// Function to flip cards
+function flip() {
+  card.classList.add("flip");
+}
+card.addEventListener("click", flip);
